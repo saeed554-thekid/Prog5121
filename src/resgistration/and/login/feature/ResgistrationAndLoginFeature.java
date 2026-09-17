49dfc3923f77eab7e78af6419891bb327223d0a9
saeed544-thekid <saeedbr034@gmail.com>
2026-09-17 12:45:00 +0200

    String loginStatusMessage = login.returnLoginStatus(loginUsername, loginPassword);        System.out.println(loginStatusMessage);

--- src/resgistration/and/login/feature/ResgistrationAndLoginFeature.java
+++ src/resgistration/and/login/feature/ResgistrationAndLoginFeature.java
@@ -6,11 +6,12 @@ public class ResgistrationAndLoginFeature {
 
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
+      Login login = new Login();
       //Username being created 
       System.out.print("Enter username: ");
       String username = input.nextLine();
       
-    while(!username.contains("_") || username.length() > 5){
+    while(!login.checkUserName(username)){
         System.out.println("Username is not correctly formatted; "
                 + "please ensure that your username contains an underscore, "
                 + "and is more more than 5 characters in length");
@@ -23,8 +24,7 @@ public class ResgistrationAndLoginFeature {
       //Password being created 
       System.out.print("Enter password: ");
        String password = input.nextLine();
-     while(password.length() < 8 || !password.matches(".*[A-Z].*") 
-             || !password.matches(".*[0-9].*") || !password.matches(".*[^a-zA-Z0-9].*")){
+     while(login.checkPasswordComplexity(password)){
          System.out.println("Passowrd is not correctly formatted, "
                  + "please ensure the password contains atleast 8 characters, "
                  + "a capital letter, a number and a special character");
@@ -38,7 +38,7 @@ public class ResgistrationAndLoginFeature {
      System.out.print("Enter a South African cellphone: ");
      String cellphone = input.nextLine();
      //the "\\" ensures that the phone number must start with +27 and the {9} allows only exactly 9 digits
-     while(!cellphone.matches("\\+27[0-9]{1,10}")){
+     while(!login.checkPhoneNumber(cellphone)){
          System.out.println("Cellphone number is incorrectly formatted"
                  + " or  does not an international code");
          System.out.print("Enter cellphone number again: ");
@@ -54,19 +54,18 @@ public class ResgistrationAndLoginFeature {
      System.out.print("Enter last name: ");
      String lastname = input.nextLine();
      
-     
+     String registrationMessage = login.registerUser(username, password, cellphone, firstname, lastname);
+     System.out.println(registrationMessage);
+             
      //User entering his username and password to login 
      System.out.println("Enter username to log back in ");
        String loginUsername = input.nextLine();
        
        System.out.println("Enter password to log back in");
        String loginPassword = input.nextLine();
-   if(username.equals(loginUsername) && password.equals(loginPassword)){
-       System.out.print("Welcome "  + firstname + ", " + lastname +   " it is great to see you again." );
-   }
-   else{
-       System.out.print("Username or password incorrect, please try again");
-   }
+    
+       String loginStatusMessage = login.returnLoginStatus(loginUsername, loginPassword);
+       System.out.println(loginStatusMessage);
     }
     
 }
